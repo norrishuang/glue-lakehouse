2d925cf3805659f603562603b2e536404c06b391
xiohuang <xiohuang@amazon.com>
2023-04-17 13:29:23 +0800

    init

---
+++ Iceberg/kafka-iceberg-streaming-glue.py
@@ -0,0 +1,279 @@
+import sys
+
+from awsglue.utils import getResolvedOptions
+from pyspark.sql.session import SparkSession
+from awsglue.context import GlueContext
+
+from awsglue.job import Job
+from datetime import datetime
+from awsglue import DynamicFrame
+from pyspark.sql.functions import col, from_json, schema_of_json, current_timestamp
+from pyspark.sql.types import StructType, StructField, StringType, LongType
+
+'''
+Glue Iceberg
+通过 Glue 消费 MSK/MSK Serverless 的数据，写S3（Iceberg）。多表，支持I U D
+
+1. 支持多表，通过MSK Connect 将数据库的数据CDC到MSK后，使用 [topics] 配置参数，可以接入多个topic的数据。
+2. 支持MSK Serverless IAM认证，需要提前在Glue Connection配置MSK的connect。MSK Connect 配置在私有子网中，私有子网配置NAT访问公网
+3. Job 参数说明
+    (1). starting_offsets_of_kafka_topic: 'latest', 'earliest'
+    (2). topics: 消费的Topic名称，如果消费多个topic，之间使用逗号分割（,）,例如 kafka1.db1.topica,kafka1.db2.topicb
+    (3). icebergdb: 数据写入的iceberg database名称
+    (4). warehouse: iceberg warehouse path
+
+'''
+## @params: [JOB_NAME]
+args = getResolvedOptions(sys.argv, ['JOB_NAME',
+                                     'starting_offsets_of_kafka_topic',
+                                     'topics',
+                                     'icebergdb',
+                                     'warehouse'])
+
+'''
+获取Glue Job参数
+'''
+STARTING_OFFSETS_OF_KAFKA_TOPIC = args.get('starting_offsets_of_kafka_topic', 'latest')
+TOPICS = args.get('topics')
+ICEBERG_DB = args.get('icebergdb')
+WAREHOUSE = args.get('warehouse')
+
+
+
+config = {
+    # "table_name": "iceberg_portfolio_kc",
+    "database_name": ICEBERG_DB,
+    "warehouse": WAREHOUSE,
+    "dynamic_lock_table": "datacoding_iceberg_lock_table",
+    "streaming_db": "kafka_db",
+    "streaming_table": "kafka_iceberg_norrisdb_01"
+}
+
+spark = SparkSession.builder \
+    .config("spark.sql.extensions","org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions") \
+    .config("spark.sql.catalog.glue_catalog","org.apache.iceberg.spark.SparkCatalog") \
+    .config("spark.sql.catalog.glue_catalog.warehouse", config['warehouse']) \
+    .config("spark.sql.catalog.glue_catalog.catalog-impl", "org.apache.iceberg.aws.glue.GlueCatalog") \
+    .config("spark.sql.catalog.glue_catalog.io-impl", "org.apache.iceberg.aws.s3.S3FileIO")\
+    .config("spark.sql.ansi.enabled", "false") \
+    .config("spark.sql.iceberg.handle-timestamp-without-timezone", True)\
+    .getOrCreate()
+sc = spark.sparkContext
+glueContext = GlueContext(sc)
+
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+logger = glueContext.get_logger()
+
+logger.info("Init...")
+
+logger.info("starting_offsets_of_kafka_topic:" + STARTING_OFFSETS_OF_KAFKA_TOPIC)
+logger.info("topics:" + TOPICS)
+logger.info("icebergdb:" + ICEBERG_DB)
+logger.info("warehouse:" + WAREHOUSE)
+
+
+def writeJobLogger(logs):
+    logger.info(args['JOB_NAME'] + " [custom log]:{0}".format(logs))
+
+# S3 sink locations
+# output_path = "s3://myemr-bucket-01/data/"
+# job_time_string = datetime.now().strftime("%Y%m%d%")
+# s3_target = output_path + job_time_string
+checkpoint_location = args["TempDir"] + "/" + args['JOB_NAME'] + "/checkpoint/" + "20230409-02" + "/"
+
+# 把 dataframe 转换成字符串，在logger中输出
+def getShowString(df, n=10, truncate=True, vertical=False):
+    if isinstance(truncate, bool) and truncate:
+        return df._jdf.showString(n, 10, vertical)
+    else:
+        return df._jdf.showString(n, int(truncate), vertical)
+
+def processBatch(data_frame, batchId):
+    if (data_frame.count() > 0):
+        schema = StructType([
+            StructField("before", StringType(), True),
+            StructField("after", StringType(), True),
+            StructField("source", StringType(), True),
+            StructField("op", StringType(), True),
+            StructField("ts_ms", LongType(), True),
+            StructField("transaction", StringType(), True)
+        ])
+
+        dataJsonDF = data_frame.select(from_json(col("$json$data_infer_schema$_temporary$").cast("string"), schema).alias("data")).select(col("data.*"))
+        writeJobLogger("############  Create DataFrame  ############### \r\n" + getShowString(dataJsonDF,truncate = False))
+
+        '''
+        由于Iceberg没有主键，需要通过SQL来处理upsert的场景，需要识别CDC log中的 I/U/D 分别逻辑处理
+        '''
+        dataInsert = dataJsonDF.filter("op in ('r','c') and after is not null")
+        # 过滤 区分 insert upsert delete
+        dataUpsert = dataJsonDF.filter("op in ('u') and after is not null")
+
+        dataDelete = dataJsonDF.filter("op in ('d') and before is not null")
+
+        if(dataInsert.count() > 0):
+            #### 分离一个topics多表的问题。
+            # dataInsert = dataInsertDYF.toDF()
+            sourceJson = dataInsert.select('source').first()
+            schemaSource = schema_of_json(sourceJson[0])
+
+            # 获取多表
+            datatables = dataInsert.select(from_json(col("source").cast("string"),schemaSource).alias("SOURCE")) \
+                .select(col("SOURCE.db"),col("SOURCE.table")).distinct()
+            # logger.info("############  MutiTables  ############### \r\n" + getShowString(dataTables,truncate = False))
+            rowtables = datatables.collect()
+
+            for cols in rowtables:
+                tableName = cols[1]
+                dataDF = dataInsert.select(col("after"),
+                                           from_json(col("source").cast("string"), schemaSource).alias("SOURCE")) \
+                    .filter("SOURCE.table = '" + tableName + "'")
+                datajson = dataDF.select('after').first()
+                schemadata = schema_of_json(datajson[0])
+                writeJobLogger("############  Insert Into-GetSchema-FirstRow:" + datajson[0])
+
+                dataDFOutput = dataDF.select(from_json(col("after").cast("string"),schemadata).alias("DFADD")).select(col("DFADD.*"), current_timestamp().alias("ts"))
+                # logger.info("############  INSERT INTO  ############### \r\n" + getShowString(dataDFOutput,truncate = False))
+                InsertDataLake(tableName, dataDFOutput)
+
+        if(dataUpsert.count() > 0):
+            #### 分离一个topics多表的问题。
+            sourcejson = dataUpsert.select('source').first()
+            schemasource = schema_of_json(sourcejson[0])
+
+            # 获取多表
+            datatables = dataUpsert.select(from_json(col("source").cast("string"), schemasource).alias("SOURCE")) \
+                .select(col("SOURCE.db"),col("SOURCE.table")).distinct()
+            # logger.info("############  MutiTables  ############### \r\n" + getShowString(dataTables,truncate = False))
+            rowtables = datatables.collect()
+
+            for cols in rowtables:
+                tableName = cols[1]
+                dataDF = dataUpsert.select(col("after"),
+                                           from_json(col("source").cast("string"), schemasource).alias("SOURCE")) \
+                    .filter("SOURCE.table = '" + tableName + "'")
+
+                ##由于merge into schema顺序的问题，这里schema从表中获取（顺序问题待解决）
+                database_name = config["database_name"]
+                # table_name = tableIndexs[tableName]
+                schemaData = spark.table(f"glue_catalog.{database_name}.{tableName}").schema
+                # dataJson = dataDF.select('after').first()
+                # schemaData = schema_of_json(dataJson[0])
+
+                dataDFOutput = dataDF.select(from_json(col("after").cast("string"),schemaData).alias("DFADD")).select(col("DFADD.*"), current_timestamp().alias("ts"))
+                writeJobLogger("############  MERGE INTO  ############### \r\n" + getShowString(dataDFOutput,truncate = False))
+                MergeIntoDataLake(tableName, dataDFOutput)
+
+
+        if(dataDelete.count() > 0):
+            # dataDelete = dataDeleteDYF.toDF()
+            sourceJson = dataDelete.select('source').first()
+            # schemaData = schema_of_json([rowjson[0]])
+
+            schemaSource = schema_of_json(sourceJson[0])
+            dataTables = dataDelete.select(from_json(col("source").cast("string"),schemaSource).alias("SOURCE")) \
+                .select(col("SOURCE.db"),col("SOURCE.table")).distinct()
+
+            # logger.info("############  Auto Schema Recognize  ############### \r\n" + getShowString(dataDelete,truncate = False))
+
+            rowTables = dataTables.collect()
+            for cols in rowTables :
+                tableName = cols[1]
+                dataDF = dataDelete.select(col("before"),
+                                           from_json(col("source").cast("string"),schemaSource).alias("SOURCE")) \
+                    .filter("SOURCE.table = '" + tableName + "'")
+                dataJson = dataDF.select('before').first()
+
+                schemaData = schema_of_json(dataJson[0])
+                dataDFOutput = dataDF.select(from_json(col("before").cast("string"),schemaData).alias("DFDEL")).select(col("DFDEL.*"))
+                # logger.info("############  DELETE FROM  ############### \r\n" + getShowString(dataDFOutput,truncate = False))
+                DeleteDataFromDataLake(tableName,dataDFOutput)
+
+def InsertDataLake(tableName,dataFrame):
+    # logger.info("##############  Func:InputDataLake [ "+ tableName +  "] ############# \r\n"
+    #             + getShowString(dataFrame,truncate = False))
+
+    database_name = config["database_name"]
+    # table_name = tableIndexs[tableName]
+
+    # partition as id
+    dyDataFrame = DynamicFrame.fromDF(dataFrame, glueContext, "from_data_frame").toDF().repartition(4, col("id"));
+
+    ###如果表不存在，创建一个空表
+    # TempTable = "tmp_" + tableName + "_upsert"
+    # dyDataFrame.createOrReplaceTempView(TempTable)
+    '''
+    如果表不存在，新建。解决在 writeto 的时候，空表没有字段的问题。
+    write.spark.accept-any-schema 用于在写入 DataFrame 时，Spark可以自适应字段。
+    format-version 使用iceberg v2版本
+    '''
+    creattbsql = f"""CREATE TABLE IF NOT EXISTS glue_catalog.{database_name}.{tableName} 
+          USING iceberg 
+          TBLPROPERTIES ('write.distribution-mode'='hash',
+          'format-version'='2',
+          'write.spark.accept-any-schema'='true')"""
+    writeJobLogger("####### IF table not exists, create it:" + creattbsql)
+    spark.sql(creattbsql)
+
+    dyDataFrame.writeTo(f"glue_catalog.{database_name}.{tableName}") \
+        .option("merge-schema", "true") \
+        .option("check-ordering", "false").append()
+
+def MergeIntoDataLake(tableName,dataFrame):
+
+    # logger.info("##############  Func:MergeIntoDataLake [ "+ tableName +  "] ############# \r\n"
+    #             + getShowString(dataFrame,truncate = False))
+    database_name = config["database_name"]
+    # table_name = tableIndexs[tableName]
+    dyDataFrame = DynamicFrame.fromDF(dataFrame, glueContext, "from_data_frame").toDF();
+
+    TempTable = "tmp_" + tableName + "_upsert"
+    dyDataFrame.createOrReplaceTempView(TempTable)
+
+    query = f"""MERGE INTO glue_catalog.{database_name}.{tableName} t USING (SELECT * FROM {TempTable}) u ON t.ID = u.ID
+            WHEN MATCHED THEN UPDATE
+                SET *
+            WHEN NOT MATCHED THEN INSERT * """
+    logger.info("####### Execute SQL:" + query)
+    spark.sql(query)
+
+def DeleteDataFromDataLake(tableName,dataFrame):
+    database_name = config["database_name"]
+    # table_name = tableIndexs[tableName]
+    dyDataFrame = DynamicFrame.fromDF(dataFrame, glueContext, "from_data_frame").toDF()
+    dyDataFrame.createOrReplaceTempView("tmp_" + tableName + "_delete")
+    query = f"""DELETE FROM glue_catalog.{database_name}.{tableName} AS t1 
+        where EXISTS (SELECT ID FROM tmp_{tableName}_delete WHERE t1.ID = ID)"""
+    # {"data":{"id":1,"reward":10,"channels":"['email', 'mobile', 'social']","difficulty":"10","duration":"7","offer_type":"bogo","offer_id":"ae264e3637204a6fb9bb56bc8210ddfd"},"op":"+I"}
+    spark.sql(query)
+# Script generated for node Apache Kafka
+kafka_options = {
+    "connectionName": "msk-serverless-connector",
+    "topicName": TOPICS,
+    "inferSchema": "true",
+    "classification": "json",
+    "startingOffsets": STARTING_OFFSETS_OF_KAFKA_TOPIC,
+    "kafka.security.protocol": "SASL_SSL",
+    "kafka.sasl.mechanism": "AWS_MSK_IAM",
+    "kafka.sasl.jaas.config": "software.amazon.msk.auth.iam.IAMLoginModule required;",
+    "kafka.sasl.client.callback.handler.class": "software.amazon.msk.auth.iam.IAMClientCallbackHandler"
+}
+
+# Script generated for node Apache Kafka
+dataframe_ApacheKafka_source = glueContext.create_data_frame.from_options(
+    connection_type="kafka",
+    connection_options=kafka_options
+)
+
+glueContext.forEachBatch(frame=dataframe_ApacheKafka_source,
+                         batch_function=processBatch,
+                         options={
+                             "windowSize": "15 seconds",
+                             "recordPollingLimit": "50000",
+                             "checkpointLocation": checkpoint_location,
+                             "batchMaxRetries": 1
+                         })
+
+job.commit()
---
+++ readme.md
@@ -0,0 +1,51 @@
+# AWS Glue 
+
+
+
+AWS Glue 4.0 版本已经支持Hudi，Iceberg，DetlaLake
+
+**版本说明**
+
+| Data Lake  | Glue 4.0 | Glue 3.0 |
+| ---------- | -------- | -------- |
+| Hudi       | 0.12.1   | 0.10.1   |
+| Iceberg    | 1.0.0    | 0.13.1   |
+| Delta Lake | 2.1.0    | 1.0.0    |
+
+
+
+#### Hudi
+
+- 可以轻松更改 Hudi 表的Schema，以适应不断变化的数据模式。通过ALTER TABLE语法为 Spark 3.1.x 和 Spark 3.2.1 添加了 Spark SQL DDL 支持（实验性）
+- Hudi 表可以直接通过 AWS 开发工具包同步到 AWS Glue Data Catalog。用户可以设置 org.apache.hudi.aws.sync.AwsGlueCatalogSyncTool为HoodieDeltaStreamer 的同步工具实现，并使 Hudi 表在 Glue Catalog中可被发现。
+- Hudi 表的元数据（特别是模式和上次同步提交时间）可以同步到DataHub。用户可以将目标表设置org.apache.hudi.sync.datahub.DataHubSyncTool为HoodieDeltaStreamer的同步工具实现，并将目标表同步为DataHub中的Dataset。
+- 添加了对 Spark 3.2 的支持，并附带了 Parquet 1.12， 支持Hudi（COW表）加密功能。
+
+#### Iceberg
+
+- merge-on-read 支持通过 MERGE and UPDATE SQL语法（Spark 3.2 或更高版本）
+
+- - 通过merge into 支持upserts。使用直接复制-写的方式。需要更新的文件会立即被重写。
+
+- 在 Spark 3.3版本中,支持 通过 `AS OF` 语法查询time travel
+
+- - Spark读取Iceberg表可以指定“***as-of-timestamp***”参数，通过指定一个毫秒时间参数查询Iceberg表中数据，iceberg会根据元数据找出***timestamp-ms <= as-of-timestamp 对应的 snapshot-id***
+
+- 新增支持通过DataFrame写操作 [`mergeSchema`](https://github.com/apache/iceberg/pull/4154)
+
+- - 设置TableProperties:”write.spark.accept-any-schema”为true。Spark默认在plan的时候会检查写入的DataFrame和表的schema是否匹配，不匹配就抛出异常。所以需要增加一个TableCapability（TableCapability.ACCEPT_ANY_SCHEMA），这样Spark就不会做这个检查，交由具体的DataSource来检查。`df.writeTo(tableName).option(“merge-schema”, “true”).XX`
+
+#### DeltaLake
+
+- **2.1.0 版本开始支持 Apache Spark 3.3.**
+- **支持SQL中的[TIMESTAMP | VERSION] AS OF。在** Spark 3.3 中，Delta现在支持*SQL中的[时间旅行](https://docs.delta.io/2.1.0/delta-batch.html#query-an-older-snapshot-of-a-table-time-travel)* ，方便查询历史版本的数据。时间旅行在SQL中和通过DataFrame API都可以使用。
+- **当通过流式数据写入 Delta table时，支持 Trigger.AvailableNow 。**Spark 3.3 引入了[Trigger.AvailableNow](https://issues.apache.org/jira/browse/SPARK-36533)，在对Delta表流式读诗，用于在多个批次中运行像Trigger.Once这样的流式查询。
+
+- - **支持 SHOW COLUMNS** 可返回表中的列**.**
+  - **在Scala 和 Python DeltaTable API 中支持** [**DESCRIBE DETAIL**](https://docs.delta.io/2.1.0/delta-utility.html#retrieve-delta-table-details) **。** 使用DeltaTable API和SQL检索Delta表的详细信息。
+
+- **支持从 SQL [Delete](https://github.com/delta-io/delta/pull/1328)，[Merge](https://github.com/delta-io/delta/pull/1327)，[Update](https://github.com/delta-io/delta/pull/1331) 命令返回操作指标。**以前这些SQL命令返回一个空的数据框架，现在它们返回一个带有关于所执行操作的有用指标的数据框架。
+- **优化性能改进**
+
+[添加了一个配置](https://docs.delta.io/2.1.0/optimizations-oss.html#compaction-bin-packing)，在 Optimize 中使用 repartition(1) 而不是 coalesce(1) ，以便在压缩许多小文件时获得更好的性能。
+通过使用基于队列的方法来并行化压缩工作，[提高 Optimize 的性能](https://github.com/delta-io/delta/pull/1315)
